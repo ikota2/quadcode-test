dc5627a3a794dd2962f6002a1529209c6ea51113
Ivan Kotov <polyzay@gmail.com>
2025-08-04 22:08:09 +0200

    feat: change csr to ssr

--- src/lib/components/HeaderSection.svelte
+++ src/lib/components/HeaderSection.svelte
@@ -1,55 +1,27 @@
-<script lang="ts">
-	import { onMount } from 'svelte';
-	import { countryCode } from '$lib/stores/geo';
-	import Title from './Title.svelte';
-	import Subtitle from './Subtitle.svelte';
-	import Button from './Button.svelte';
-
-	$: showButton = $countryCode !== 'US';
-
-	onMount(async () => {
-		try {
-			const response = await fetch('https://ipapi.co/json/');
-			const data = await response.json();
-			document.cookie = `country_code=${data.country_code}; max-age=604800; path=/`;
-			countryCode.set(data.country_code);
-		} catch (error) {
-			console.error('API failed:', error);
-		}
-	});
+<script>
+  import Title from './Title.svelte';
+  import Subtitle from './Subtitle.svelte';
 </script>
 
 <section class="header-section">
-	<div class="title-wrapper">
-		<Title text="Instant access to investing, anytime and anywhere" />
-	</div>
-	<Subtitle
-		text="Start trading top assets, from stocks to metals, in minutes, right from your phone or laptop."
-	/>
-	<Subtitle
-		text="Enjoy a complete trading ecosystem with built-in analytics and educational tools."
-	/>
-	<div class="button-wrapper">
-		{#if showButton}
-			<Button />
-		{:else if $countryCode === 'US'}
-			<Subtitle text="Unfortunately, service is not available in your region" />
-		{/if}
-	</div>
+  <div class="title-wrapper">
+    <Title text="Instant access to investing, anytime and anywhere" />
+  </div>
+  <Subtitle
+    text="Start trading top assets, from stocks to metals, in minutes, right from your phone or laptop."
+  />
+  <Subtitle
+    text="Enjoy a complete trading ecosystem with built-in analytics and educational tools."
+  />
 </section>
 
 <style>
-	.header-section {
-		max-width: 1000px;
-		text-align: center;
-		padding: 2rem 1rem;
-		margin: 0 auto;
-	}
-	.button-wrapper {
-		height: 50px;
-		padding: 32px 0;
-	}
-	.title-wrapper {
-		padding: 32px 0;
-	}
+  .header-section {
+    max-width: 1000px;
+    text-align: center;
+    margin: 0 auto;
+  }
+  .title-wrapper {
+    padding: 32px 0;
+  }
 </style>
